d079ec3c8d31c8be1e5f2ed9f1ada935b08d89e9
Mukul Kolpe <mukulkolpe45@gmail.com>
2023-12-28 22:44:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Auth } from "./components/Auth";
+
+function App() {
+  return (
+    <div className="App">
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
